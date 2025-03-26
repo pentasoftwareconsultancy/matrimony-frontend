d2b2d8b186aa6211ed9a09c91d723e85c5ced30b
sonu2404 <sonaliekhande12@gmail.com>
2025-03-26 15:53:06 +0530

    map

--- src/page/contactpages/Contactpages.jsx
+++ src/page/contactpages/Contactpages.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Contact from '../../components/contacts/contacts/Contact'
-import Map from '../../Components/contacts/map/Map'
+import Map from '../../components/contacts/maps/Map'
 // import Map from '../../contacts/map/map'
 // import Map from "../../contacts/map/Map"
 function Contactpages() {
