9ac78c37cafbea2d68dd4d28d9c2516d775b2859
sonu2404 <sonaliekhande12@gmail.com>
2025-03-26 15:56:19 +0530

    new

--- src/page/newspages/Newspages.jsx
+++ src/page/newspages/Newspages.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import Card from "../../Components/newscard/card/Card";
-import Newshero from "../../Components/newscard/newshero/Newshero";
-import Newsfilter from "../../Components/newscard/newsfilter/Newsfilter";
-import Newcard from "../../Components/newscard/nescard/Newcard";
+// import React from "react";
+// import Card from "../../Components/newscard/card/Card";
+// import Newshero from "../../Components/newscard/newshero/Newshero";
+// import Newsfilter from "../../Components/newscard/newsfilter/Newsfilter";
+// import Newcard from "../../Components/newscard/nescard/Newcard";
 
 
-const Newpages = () => {
-  return<>
-  <Newshero/>
-  <Newsfilter/>
+// const Newpages = () => {
+//   return<>
+//   <Newshero/>
+//   <Newsfilter/>
 
-<Card/>
-{/* <Newcard/> */}
-  </> 
+// <Card/>
+// <Newcard/>
+//   </> 
   
-};
+// };
 
-export default Newpages;
+// export default Newpages;
