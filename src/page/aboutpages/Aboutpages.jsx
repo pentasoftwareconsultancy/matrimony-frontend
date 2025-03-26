464a6eaaddab2c0b634ec0fa823a2c44c6fdafee
sonu2404 <sonaliekhande12@gmail.com>
2025-03-26 15:49:05 +0530

    section

--- src/page/aboutpages/Aboutpages.jsx
+++ src/page/aboutpages/Aboutpages.jsx
@@ -4,7 +4,7 @@ import Section from '../../components/about/section/Section'
 import Media from '../../components/about/medias/Media'
 import Aboutus from '../../components/about/aboutuss/Aboutus'
 import Gallary from '../../components/about/gallarys/Gallary'
-import Sectionmain from '../../Components/about/sectionmain/Sectionmain'
+import Sectionmain from '../../components/about/sectionmains/Sectionmain'
 import Testimonials from '../../components/home/testimonials/Testimonials'
 
 function Aboutpages() {
