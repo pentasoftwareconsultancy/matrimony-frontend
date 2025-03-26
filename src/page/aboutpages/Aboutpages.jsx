4d4d69fadc3e0242b36173d8fa3fa6ae1e1d87eb
sonu2404 <sonaliekhande12@gmail.com>
2025-03-26 15:24:35 +0530

    about

--- src/page/aboutpages/Aboutpages.jsx
+++ src/page/aboutpages/Aboutpages.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Abouthero from '../../Components/about/abouthero/Abouthero'
-import Section from '../../Components/about/abouthero/section/Section'
+import Section from '../../components/about/section/Section'
 import Media from '../../Components/about/media/Media'
 import Aboutus from '../../Components/about/aboutus/Aboutus'
 import Gallary from '../../Components/about/gallary/Gallary'
