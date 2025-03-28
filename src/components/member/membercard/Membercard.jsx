2705d2420315057d59df7385cf15a79a2c234854
sonu2404 <sonaliekhande12@gmail.com>
2025-03-28 12:19:13 +0530

    member

--- src/components/member/membercard/Membercard.jsx
+++ src/components/member/membercard/Membercard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./MemberCard.module.css";
+import styles from "./Membercard.module.css";
 
 const MemberCard = ({ member }) => {
   return (
